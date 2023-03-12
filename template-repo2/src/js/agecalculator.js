b0bac8b08834ad61aa18a57ad91ccf639861270a
Emiliano Slaibe <emslaibe@gmail.com>
2023-03-12 18:10:57 -0400

    it pass jupiter age calculation

--- template-repo2/src/js/agecalculator.js
+++ template-repo2/src/js/agecalculator.js
@@ -8,14 +8,19 @@ export default class AgeCalculator {
     return this.age / 0.24;
   }
 
-    // Returns the age of the person in Venus years
+  // Returns the age of the person in Venus years
   get venusAge() {
     return this.age / 0.62;
   }
 
-    // Returns the age of the person in Mars years
+  // Returns the age of the person in Mars years
   get marsAge() {
     return this.age / 1.88;
+  }
+
+  // Returns the age of the person in Jupiter years
+  get jupiterAge() {
+    return this.age / 11.86;
   }
 
 }
