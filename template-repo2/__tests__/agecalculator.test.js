2ec089b87dce6fb48cd1499dbae13c05361a4211
Emiliano Slaibe <emslaibe@gmail.com>
2023-03-12 17:50:38 -0400

    it fail to calculate venus age

--- template-repo2/__tests__/agecalculator.test.js
+++ template-repo2/__tests__/agecalculator.test.js
@@ -5,4 +5,10 @@ describe('AgeCalculator', () => {
     const ageCalculator = new AgeCalculator(24);
     expect(ageCalculator.mercuryAge).toBe(100);
   });
+
+  it('calculates venus age correctly', () => {
+    const ageCalculator = new AgeCalculator(24);
+    expect(ageCalculator.venusAge).toBeCloseTo(38.71, 2);
+  });
+
 });
