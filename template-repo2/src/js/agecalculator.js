9473bb9445c0b9762abb49d650344aba5cfad3fd
Emiliano Slaibe <emslaibe@gmail.com>
2023-03-12 17:45:43 -0400

    it pass the test for calculating mercury age correctly

--- template-repo2/src/js/agecalculator.js
+++ template-repo2/src/js/agecalculator.js
@@ -1,5 +1,10 @@
 export default class AgeCalculator {
-  // constructor(age) {
-  //   this.age = age;
-  // }
+  constructor(age) {
+    this.age = age;
+  }
+
+  // Returns the age of the person in Mercury years
+  get mercuryAge() {
+    return this.age / 0.24;
+  }
 }
